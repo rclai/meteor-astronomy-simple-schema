22cbd151d6223964c335db3d13ecda88e45bf0f5
Richard Lai <it.rclai@gmail.com>
2015-10-10 23:37:40 -0400

    Add package.js metadata

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'lai:astronomy-simple-schema',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Convert Astronomy classes to SimpleSchemas',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rclai/meteor-astronomy-simple-schema.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
